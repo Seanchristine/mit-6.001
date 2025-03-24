bc4e1c5af62adc8f216c3c1f4540e5efc0c9f445
Seanchristine <a844527052@gmail.com>
2025-03-24 15:49:27 +0800

    Add files via upload

---
+++ pset0.py
@@ -0,0 +1,8 @@
+# pset0
+from math import log2
+x=int(input("please enter a number:"))
+y=int(input("please enter another number:"))
+xpowery=x**y
+log=log2(x)
+print("x power y is",xpowery)
+print("log(x) is",log)
